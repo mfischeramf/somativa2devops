48b2edc76d16811139e3715ad8e8683f8a5e701b
mfischeramf <86022226+mfischeramf@users.noreply.github.com>
2023-04-16 17:52:00 -0300

    Add files via upload

--- web/main.py
+++ web/main.py
@@ -1,23 +1,23 @@
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
-    
     if request.method == 'POST':
         num1 = request.form['num1'].upper()
         num2 = request.form['num2'].upper()
-        numr = float(num1)+float(num2)
+        numr = float(num1) + float(num2)
         print(numr)
 
 
     else:
         num1 = ''
         num2 = ''
+        numr = ''
 
-
-    return render_template("homepage.html", resultado = numr)
-
-
+    return render_template("homepage.html", resultado=numr)
 
 
 if __name__ == "__main__":
